5986ffb12bed35290dbfe315ec8222460158e2f3
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2023-12-05 21:51:04 +0530

    clear database

--- project/settings.py
+++ project/settings.py
@@ -139,18 +139,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-# import os
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
-# STATICFILES_DIRS = [
-#     'project/static',
-# ] 
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR/'media'
-# ################################################
 
 
 import os
