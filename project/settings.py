644ee98416f1d26c382eb6f5dc780e24549de853
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2024-06-26 18:58:18 +0530

    razorpay updated

--- project/settings.py
+++ project/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Use os.path.join to construct the path
+    os.path.join(BASE_DIR, 'static/'),  # Use os.path.join to construct the path
 ]
 
 MEDIA_URL = '/media/'
